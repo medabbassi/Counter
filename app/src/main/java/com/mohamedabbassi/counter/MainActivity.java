0cf7e7e93d089eaeadb471e1c022b2598cc7dfe7
medex3 <mabbassix3@gmail.com>
2018-11-11 21:58:55 +0100

    Layout of activity

--- app/src/main/java/com/mohamedabbassi/counter/MainActivity.java
+++ app/src/main/java/com/mohamedabbassi/counter/MainActivity.java
@@ -3,18 +3,28 @@ package com.mohamedabbassi.counter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+        private int mCount =0;
+        private TextView mShowCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount=findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
+        Toast.makeText(this, "Hello you press button:\t"+mCount, Toast.LENGTH_SHORT).show();
     }
 
     public void countUp(View view) {
+        mCount++;
+        if (mShowCount !=null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
